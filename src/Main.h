63209add55d00fb5c9f5232b770abe4828684b0f
Alwin Esch <alwin.esch@web.de>
2026-05-31 17:58:31 +0200

    addon: Use ProjectM logging API functions and callbacks
    Implemented in ProjectM on Dec 7, 2025 with c6e02c9
    Not included in the 4.1.6 release

--- src/Main.h
+++ src/Main.h
@@ -44,15 +44,15 @@ d4rk@xbmc.org
 
 #include <atomic>
 #include <kodi/addon-instance/Visualization.h>
-#include <projectM-4/projectM.h>
-#include <projectM-4/playlist.h>
 #include <mutex>
+#include <projectM-4/playlist.h>
+#include <projectM-4/projectM.h>
 
 class ATTR_DLL_LOCAL CVisualizationProjectM : public kodi::addon::CAddonBase,
                                               public kodi::addon::CInstanceVisualization
 {
 public:
-  CVisualizationProjectM() = default;
+  CVisualizationProjectM();
   ~CVisualizationProjectM() override;
 
   bool Init() override;
@@ -76,6 +76,9 @@ private:
   std::string GetBasename(std::string fullPath);
   void ReloadPlaylist();
   static void PresetSwitchedEvent(bool isHardCut, unsigned int index, void* context);
+  static void ProjectMLogCallback(const char* message,
+                                  projectm_log_level log_level,
+                                  void* user_data);
 
   bool m_settingChanged{true};
   bool m_UserPackFolder{false};
